351ed6a8c1e7db01444c517cda0d5c959cb2d1cf
Akshay Badagabettu <74955326+akshay140601@users.noreply.github.com>
2023-03-08 11:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -825,6 +825,7 @@ if feed_actuation == "Cylinder feed":
             lc4a_stress = pulldown_prediction_print_LL + ' - ' + pulldown_prediction_print_UL
             lc4b_stress = pullback_prediction_print_LL + ' - ' + pullback_prediction_print_UL
             cd_loc1 = cd_loc_1_LL + '-' + cd_loc_1_UL
+            mast_weight = mast_weight/0.453592
             
             html = template.render(
                 mast_width = mast_width,
@@ -1553,6 +1554,7 @@ elif feed_actuation == 'Motor feed':
             lc3b_stress = extending_prediction_print_LL + ' - ' + extending_prediction_print_UL
             lc4b_stress = pullback_prediction_print_LL + ' - ' + pullback_prediction_print_UL
             cd_loc1 = cd_loc_1_LL + '-' + cd_loc_1_UL
+            mast_weight = mast_weight/0.453592
             
             html = template.render(
                 mast_width = mast_width,
